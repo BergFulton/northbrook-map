302062520311f913115bf6abd2a801d1707d41e5
BergFulton <BergFulton@users.noreply.github.com>
2016-09-27 14:29:46 -0400

    Fix map error-apparently it's the CSS

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 var map;
 var infowindow; // create global infowindow
 
+
 //Draw the map. It's centered on the LatLng for Pittsburgh, PA, USA.
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -12,14 +13,14 @@ function initMap() {
         zoom: 10,
         mapTypeControl: true
     });
+
+    // create global infowindow 
+    infowindow = new google.maps.InfoWindow();
+    ko.applyBindings(new ViewModel());
 }
 
-//     // create global infowindow 
-//     infowindow = new google.maps.InfoWindow();
-//     ko.applyBindings(new ViewModel());
-// }
+//Google Maps Error
+function googleError(){
+    alert("Google Has An Error!");
+}
 
-// //Google Maps Error
-// function googleError(){
-//     alert("Google Has An Error!");
-// }
